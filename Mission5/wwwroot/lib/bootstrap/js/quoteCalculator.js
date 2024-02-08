33184f4c32604ee9506e0dbb27c12c7f6a6ad559
christensen-dev <dogsippepsi@gmail.com>
2024-02-07 21:15:04 -0700

    Finished the project, added quote calculator and navigation between pages.

---
+++ Mission5/wwwroot/lib/bootstrap/js/quoteCalculator.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $('#quoteForm').submit(function (event) {
+        event.preventDefault();
+        calculateTotal();
+    });
+});
+
+function calculateTotal() {
+    var hours = parseFloat($('#hours').val());
+    var rate = parseFloat($('#rate').val());
+    var total = hours * rate;
+
+    if (!isNaN(total) && total >= 0) {
+        $('#totalOutput').val('Total: $' + total.toFixed(2));
+    } else {
+        $('#totalOutput').val('Please enter a valid number of hours.');
+    }
+}
